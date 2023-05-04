04d5f823e96a71487407b6692900f29ce9161bee
marcos rios <max.rios92@gmail.com>
2023-05-04 16:30:38 -0300

    backup character and run db

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ async function main () {
     server.listen(PORT, console.log(`server raised in port: http://localhost:${PORT}`));
     
     try {
-        await dbRickAndMorty.sync({force: true});
+        await dbRickAndMorty.sync({force: false});
         console.log('Connection has been established successfully.');        
     } catch (error) {
         console.log('Unable to connect to the database:', error);        
