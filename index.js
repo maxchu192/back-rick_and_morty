f91426ed43791bc93275079845d6df9e387b762f
marcos rios <max.rios92@gmail.com>
2023-05-04 17:09:46 -0300

    deploy on vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { dbRickAndMorty } = require('./src/database/DB_connection.js');
 
 const server = require('./src/server.js')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; 
 
 
 async function main () {
